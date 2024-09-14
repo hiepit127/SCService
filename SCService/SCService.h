382261e2f2f2a121a13a44d15fab0598a21b4e54
Hoàng Hiệp <hiepit127@gmail.com>
2024-09-14 10:40:23 +0700

    init project

---
+++ SCService/SCService.h
@@ -0,0 +1,18 @@
+//
+//  SCService.h
+//  SCService
+//
+//  Created by Hoàng Hiệp on 14/9/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SCService.
+FOUNDATION_EXPORT double SCServiceVersionNumber;
+
+//! Project version string for SCService.
+FOUNDATION_EXPORT const unsigned char SCServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SCService/PublicHeader.h>
+
+
